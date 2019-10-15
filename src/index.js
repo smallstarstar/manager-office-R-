b687e6146bf192e99d17ebdd61c1e63fbeea70be
shichaoxin <2751397601@qq.com>
2019-10-15 14:41:36 +0800

    搭建环境

--- src/index.js
+++ src/index.js
@@ -3,10 +3,39 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import axios from 'axios';
+import qs from 'Qs';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+/**
+ * 请求参数的拦截
+ */
+axios.interceptors.request.use((config) => {
+    const { method, data } = config;
+    if (method.toLocaleLowerCase() === 'post' && typeof data === 'object') {
+        config.data = qs.stringify(data);
+    }
+    // token的拦截
+    const token = localStorage.getItem('item');
+    if (token) {
+        token = 'bearer' + ' ' + token.replace(/'|"/g, '') // 把token加入到默认请求参数中
+
+        config.headers.common['Authorization'] = token
+    }
+    return config;
+});
+
+
+
+/**
+ * 请求相应拦截
+ */
+axios.interceptors.response.use((res) => {
+    return res.data;
+})
+
+
+ReactDOM.render( < App / > , document.getElementById('root'));
+
+
 serviceWorker.unregister();
